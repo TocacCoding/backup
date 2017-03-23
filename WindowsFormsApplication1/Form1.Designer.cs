6ebe1fdac94cd4c2ed2fb0072c522e995dce0e58
TocacCoding <tocac@hotmail.com>
2017-03-23 06:43:48 +0000

    updated

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,13 +30,20 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 400;
+            this.timer1.Interval = 600;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 180000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
@@ -53,6 +60,7 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -17,131 +17,141 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-
-        [DllImport("user32.dll")]
-        public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
-        [DllImport("user32.dll")]
-        public static extern bool SetForegroundWindow(IntPtr WindowHandle);
-        public const int SW_SHOW = 5;
-
         public Form1()
         {
             InitializeComponent();
         }
-
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Thread.Sleep(7000);
+            //MessageBox.Show(GetPixel(945, 64).ToString());
+        }
+        /// Help funcs
+        // Get pixel from screen
         int GetPixel(int X, int Y)
         {
             Bitmap bmp = new Bitmap(1, 1, PixelFormat.Format32bppPArgb);
             Graphics grp = Graphics.FromImage(bmp);
             grp.CopyFromScreen(new Point(X, Y), Point.Empty, new Size(1, 1));
             grp.Save();
-
             return bmp.GetPixel(0, 0).ToArgb();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //MessageBox.Show(GetPixel(1857, 234).ToString());
-        }
-
-        public static void focus()
-        {
-            System.Diagnostics.Process[] objProcesses = System.Diagnostics.Process.GetProcessesByName("client");
-            if (objProcesses.Length > 0)
-            {
-                IntPtr hWnd = IntPtr.Zero;
-                hWnd = objProcesses[0].MainWindowHandle;
-                SetForegroundWindow(objProcesses[0].MainWindowHandle);
-            }
-        }
-
+        }        
+        /// heal funcs
         public void red()
         {
-            focus();
             InputSimulator s = new InputSimulator();
             s.Keyboard.KeyPress(VirtualKeyCode.F12);
-            Thread.Sleep(950);
+            Thread.Sleep(955);
         }
-
         public void yellow()
         {
-            focus();
             InputSimulator s = new InputSimulator();
             s.Keyboard.KeyPress(VirtualKeyCode.F11);
-            Thread.Sleep(950);
+            Thread.Sleep(955);
         }
-
         public void green()
         {
-            focus();
             InputSimulator s = new InputSimulator();
             s.Keyboard.KeyPress(VirtualKeyCode.F10);
-            Thread.Sleep(950);
+            Thread.Sleep(955);
         }
-
-        public void ma()
+        /// mana funcs
+        public void m()
         {
-            focus();
             InputSimulator s = new InputSimulator();
             s.Keyboard.KeyPress(VirtualKeyCode.F9);
             Thread.Sleep(1250);
         }
-
         public void mw()
         {
-            focus();
             InputSimulator s = new InputSimulator();
             s.Keyboard.KeyPress(VirtualKeyCode.F3);
         }
+        /// status checker funcs
+        public bool isPZ()
+        {
+            // coords status
+            int statusAux = -13534304;
+            int X1 = 954;
+            int X2 = 964;
+            int X3 = 945;
+            int X4 = 973;
+            int X5 = 936;
+            int X6 = 982;
+            int X7 = 927;
+            int Y = 64;
 
+            if (GetPixel(X1, Y) == statusAux || GetPixel(X2, Y) == statusAux || GetPixel(X3, Y) == statusAux ||
+                GetPixel(X4, Y) == statusAux || GetPixel(X5, Y) == statusAux || GetPixel(X6, Y) == statusAux ||
+                GetPixel(X7, Y) == statusAux)
+            {
+                return true;
+            }
+            return false;
+        }
+        /// misc funcs
+        public void eat()
+        {
+            InputSimulator s = new InputSimulator();
+            s.Keyboard.KeyPress(VirtualKeyCode.F8);
+        }
+        public void test()
+        {
+            InputSimulator s = new InputSimulator();
+            s.Keyboard.KeyPress(VirtualKeyCode.F4);
+        }
+        /// core
         private void timer1_Tick(object sender, EventArgs e)
         {
             // no go zone
-            int tAux = -14991265;//-14991265;
-            int hAux = -2404529;//-2404529;
-            int mAux = -10132752;//-10132752;
-            int mmAux = -11317056;
-            // end of
-
-            int redX = 1820; //1820
-            int redY = 220;  //220
-
-            int yellowX = 1835; //1835
-            int yellowY = 220; //220
-
-            int greenX = 1850; //1850
-            int greenY = 220; //220
-
-            int mX = 1830; //1830
-            int mY = 234; //234
-
-            int mmX = 1857; //1857
-            int mmY = 234; //234
-
-            int tX = 1845; //1845
-            int tY = 151; //151
-
-            if (GetPixel(redX, redY) != hAux && GetPixel(tX, tY) == tAux)
+            int barAux = -13816270;
+            int controlAux = -15327199;
+            int cX = 1882;
+            int cY = 107;
+            // size hp/mana 745px (10% == 74px)
+            // hp starts 950-
+            int redX = 950-74-74-74-74; // 40%
+            int yellowX = 950-74-74-37; // 25%
+            int greenX = 950-74;        // 10%
+            int hY = 32;
+            // mp starts 970+
+            int mX = 970+74+74+74; // 30%
+            int mmX = 971;
+            int mY = 32;
+            // logics - higher priority UP
+            if (GetPixel(redX, hY) == barAux && GetPixel(cX, cY) == controlAux)
             {
                 red();
             }
-            else if (GetPixel(yellowX, yellowY) != hAux && GetPixel(tX, tY) == tAux)
+            else if (GetPixel(yellowX, hY) == barAux && GetPixel(cX, cY) == controlAux)
             {
                 yellow();
             }
-            else if (GetPixel(greenX, greenY) != hAux && GetPixel(tX, tY) == tAux)
+            else if (GetPixel(greenX, hY) == barAux && GetPixel(cX, cY) == controlAux)
             {
-                green();
+               green();
             }
-            else if (GetPixel(mX, mY) != mAux && GetPixel(yellowX, yellowY) == hAux && GetPixel(tX, tY) == tAux)
+            else if (GetPixel(mX, mY) == barAux && GetPixel(cX, cY) == controlAux)
             {
-                ma();
+                m();
             }
-            else if (GetPixel(mmX, mmY) == mmAux && GetPixel(yellowX, yellowY) == hAux && GetPixel(tX, tY) == tAux)
+            else if (GetPixel(mmX, mY) != barAux && GetPixel(cX, cY) == controlAux)
             {
                 mw();
             }
-
+            // end of 
         }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int cX = 1882;
+            int cY = 107;
+            int controlAux = -15327199;
+
+            if (GetPixel(cX, cY) == controlAux && isPZ() == false)
+            {
+                eat();
+            }
+        }
+        // nao apagar further down
     }
 }
